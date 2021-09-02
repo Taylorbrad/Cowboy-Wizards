34e8abac888ded2fc144ad1ea2623da9d444cc41
Jarrett <jarrettminton00@gmail.com>
2021-09-02 01:15:24 -0600

    Stuff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public SpriteAnimator spriteAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveVec = Vector2.zero;
+        moveVec.x = Input.GetAxisRaw("Horizontal");
+        moveVec.y = Input.GetAxisRaw("Vertical");
+
+        if(moveVec.magnitude > 1)
+        {
+            moveVec = moveVec.normalized;
+        }
+
+        rb.velocity = moveVec * moveSpeed;
+
+        if(moveVec != Vector2.zero)
+        {   
+            spriteAnimator.Play("Walk");
+        }
+        else
+        {
+            spriteAnimator.Play("Idle");
+        }
+
+        if(moveVec.x != 0)
+        {
+            spriteAnimator.FlipTo(moveVec.x);
+        }
+    }
+}

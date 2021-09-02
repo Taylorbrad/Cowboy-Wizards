bcea14766fdf804c8217327fdb2c17dc21de5844
Jarrett <jarrettminton00@gmail.com>
2021-09-02 14:11:48 -0600

    Aiming Gun!
    Added a gun that follows your mouse

---
+++ Assets/Scripts/GunMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        Vector2 moveVec = Vector2.zero;
+        moveVec.x = Input.GetAxisRaw("Horizontal");
+        moveVec.y = Input.GetAxisRaw("Vertical");
+
+        if(moveVec.magnitude > 1)
+        {
+            moveVec = moveVec.normalized;
+        }
+
+        Vector3 pos = transform.rotation.eulerAngles;
+
+        if(91 < pos.z && pos.z < 265){
+            sprite.flipY = true;
+        }
+        else
+        {
+            sprite.flipY = false;
+        }
+
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        
+
+
+
+        rb.velocity = moveVec * moveSpeed;
+    }
+}

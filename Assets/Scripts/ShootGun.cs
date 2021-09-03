d698804b7877dec6418fc740d646f6cb84ea748b
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2021-09-02 19:21:25 -0600

    fixed shoot animation
    I dont want to have to babysit you 24/7 jarrett

--- Assets/Scripts/ShootGun.cs
+++ Assets/Scripts/ShootGun.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class ShootGun : MonoBehaviour
 {
-    
+
     public SpriteAnimator flashAnimation;
     public SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(!flashAnimation.isPlaying){
             sprite.enabled = false;
+            flashAnimation.Play("Idle");
         }
+
         if (Input.GetMouseButtonDown(0)){
             sprite.enabled = true;
             flashAnimation.Play("Shoot");
-
             }
     }
 
